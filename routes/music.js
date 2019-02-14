760c116a325ccda83af363ce10743ce99fcdb83e
Aditya Respati <dityo.hario@gmail.com>
2019-02-14 18:31:16 +0700

    upload

--- routes/music.js
+++ routes/music.js
@@ -1,13 +1,14 @@
 const router = require('express').Router()
 const isLogin = require('../middlewares/isLogin')
 const isOwner = require('../middlewares/isOwner')
+const image = require('../helpers/images')
 const MusicController = require('../controllers/musicController')
 
 router.use(isLogin)
 router.get('/', MusicController.all)
-router.post('/', MusicController.create)
+router.post('/', image.multer.single('image'), image.sendUploadToGCS, MusicController.create)
 
-router.put('/:id', isOwner, MusicController.update)
+// router.put('/:id', isOwner, MusicController.update)
 router.delete('/:id', isOwner, MusicController.delete)
 
 module.exports = router
