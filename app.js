0dc5438dcbc95b6f8b463df58c3a6367a082ef2f
Veneciac <veneciacalista00@gmail.com>
2019-02-14 16:47:17 +0700

    get Image from title

--- app.js
+++ app.js
@@ -29,8 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/music', musicRouter)
-
+// app.use('/music', musicRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
